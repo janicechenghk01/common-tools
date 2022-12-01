68e43646dbd9ba8352e32fb44e6b00d82e2eca51
Janice Cheng <janicecheng@hk01.com>
2022-12-01 13:36:16 +0800

    chore: update doc

--- python/utils.py
+++ python/utils.py
@@ -7,9 +7,13 @@ def get_n_buckets(n=10):
     buckets.sort()
     return buckets
 
-item_list = [841385, 841760, 841167, 841494, 841046, 841478, 841768, 841061, 841253, 841524]
-# {'1': '["841091", "840966"]', '3': '["841370", "841609"]'}
+
 def generate_mock_data(item_list):
+    """
+    item_list = [841385, 841760, 841167, 841494, 841046, 841478, 841768, 841061, 841253, 841524]
+    generate_mock_data(item_list)
+    >> {'1': '["841091", "840966"]', '3': '["841370", "841609"]'}
+    """
     
     import json
     import numpy as np
@@ -21,12 +25,8 @@ def generate_mock_data(item_list):
     
     for i in pinPosList:
         arr = []
-        for j in range(2):
+        for _ in range(2):
             arr.append(str(item_list.pop(1)))
         pinItemsDict[str(i)] = json.dumps(arr)
 
     return pinItemsDict
-
-print(generate_mock_data(item_list)
-
-)
